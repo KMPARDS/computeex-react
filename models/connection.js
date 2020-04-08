0e0c496d5553a803fb202580680c1ca3aaad5b4d
zemse <sohamzemse143@gmail.com>
2020-04-08 20:42:23 +0530

    fix database connection lost issue

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,30 @@
 const mysql = require('mysql');
 
 module.exports = options => {
-  const connection = mysql.createConnection(options);
+  let connection;
 
-  connection.connect(error => {
-    if(error) throw error;
-    console.log('Connection database success!');
-  });
+  function connectToDb() {
+    console.log('Connecting to the database...');
+    connection = mysql.createConnection(options);
+
+    connection.connect(error => {
+      if(error) {
+        console.log(error);
+        setTimeout(connectToDb, 10000);
+      };
+      console.log('Connection database success!');
+    });
+
+    connection.on('error', error => {
+      console.log('Error happenned', Date.now(), error);
+      if(error.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('Reconnecting DB...');
+        connectToDb();
+      }
+    });
+  };
+
+  connectToDb();
 
   return (query, args) => new Promise(function(resolve, reject) {
     connection.query(query, args, (error, results, fields) => {
