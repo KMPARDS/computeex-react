7c62e4200469b7d508041367541d4c94946056e0
zemse <sohamzemse143@gmail.com>
2020-04-08 18:26:59 +0530

    add heroku build script

--- src/env.js
+++ src/env.js
@@ -1,3 +1,9 @@
-module.exports = {
+const env = {
   apiBaseUrl: 'http://localhost:3001/api'
 };
+
+if(process.env.REACT_APP_STAGE === 'production') {
+  env.apiBaseUrl = '/api';
+}
+
+module.exports = env;
