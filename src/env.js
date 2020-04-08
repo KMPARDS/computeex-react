f61db7c89487b82ff9b365e0960867dd17b9c53c
zemse <sohamzemse143@gmail.com>
2020-04-08 20:41:07 +0530

    fix env variables

--- src/env.js
+++ src/env.js
@@ -1,9 +1,6 @@
 const env = {
-  apiBaseUrl: 'http://localhost:3001/api'
+  apiBaseUrl: process.env.REACT_APP_BASE_API_URL || 'http://localhost:3001/api',
+  upholdClientId: process.env.REACT_APP_UPHOLD_CLIENT_ID || '6afd79abbf4d5be522fba1a9a3b65e9f10509eb9'
 };
-
-if(process.env.REACT_APP_STAGE === 'production') {
-  env.apiBaseUrl = '/api';
-}
 
 module.exports = env;
